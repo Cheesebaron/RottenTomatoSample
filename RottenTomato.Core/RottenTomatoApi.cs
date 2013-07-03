c0fdffb42499b0a8cae2467bf6a34c5b665239b6
Tomasz Cielecki <tomasz@ostebaronen.dk>
2013-07-03 23:31:17 +0200

    Yay!

---
+++ RottenTomato.Core/RottenTomatoApi.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RottenTomato.Core
+{
+    public static class RottenTomatoApi
+    {
+        private const string ApiToken = "h524hezne2gmh29nf5waqtpw";
+        private const string MovieListUrl = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey={0}&q={1}";
+
+        public static async Task<RootObject> GetMovieList(string searchTerm)
+        {
+            var client = new HttpClient();
+            var msg = await client.GetAsync(string.Format(MovieListUrl, ApiToken, searchTerm));
+            if (msg.IsSuccessStatusCode)
+            {
+                using (var stream = await msg.Content.ReadAsStreamAsync())
+                {
+                    using (var streamReader = new StreamReader(stream))
+                    {
+                        var str = await streamReader.ReadToEndAsync();
+                        var obj = await JsonConvert.DeserializeObjectAsync<RootObject>(str);
+                        return obj;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ RottenTomato.Droid/Activity1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using RottenTomato.Core;
+
+namespace RottenTomato.Droid
+{
+    [Activity(Label = "RottenTomato.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        private List<string> _titles = new List<string>();
+        private ListView _list;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            _list = FindViewById<ListView>(Resource.Id.listView1);
+
+            button.Click += async delegate
+                {
+                    var root = await RottenTomatoApi.GetMovieList("battlestar");
+                    ShowTitles(root);
+                };
+        }
+
+        private void ShowTitles(RootObject root)
+        {
+            _titles.Clear();
+            foreach (var movie in root.movies)
+            {
+                _titles.Add(movie.title);
+            }
+
+            _list.Adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, _titles);
+            _list.Invalidate();
+        }
+    }
+}
+
